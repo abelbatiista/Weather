31b24a3307feda362b1a74407cb02d68db7a886c
ABel Batista Rodríguez <AbelBatiista@outlook.com>
2021-08-25 12:27:54 -0400

    Weather

---
+++ Weather.Service/ProjectInstaller.Designer.cs
@@ -0,0 +1,62 @@
+﻿
+using System.ServiceProcess;
+
+namespace Weather.Service
+{
+    partial class ProjectInstaller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.weatherServiceProcessInstaller = new System.ServiceProcess.ServiceProcessInstaller();
+            this.weatherServiceInstaller = new System.ServiceProcess.ServiceInstaller();
+            // 
+            // weatherServiceProcessInstaller
+            // 
+            this.weatherServiceProcessInstaller.Account = ServiceAccount.LocalSystem;
+            this.weatherServiceProcessInstaller.Password = null;
+            this.weatherServiceProcessInstaller.Username = null;
+            // 
+            // weatherServiceInstaller
+            // 
+            this.weatherServiceInstaller.ServiceName = "WeatherService";
+            this.weatherServiceInstaller.DisplayName = "Weather: Service";
+            this.weatherServiceInstaller.Description = "Service to indicate the weather at Santo Domingo's City.";
+            // 
+            // ProjectInstaller
+            // 
+            this.Installers.AddRange(new System.Configuration.Install.Installer[] {
+            this.weatherServiceProcessInstaller,
+            this.weatherServiceInstaller});
+
+        }
+
+        #endregion
+
+        private System.ServiceProcess.ServiceProcessInstaller weatherServiceProcessInstaller;
+        private System.ServiceProcess.ServiceInstaller weatherServiceInstaller;
+    }
+}
---
+++ Weather.Service/WeatherService.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace Weather.Service
+{
+    public partial class WeatherService : ServiceBase
+    {
+        private readonly Timer timer;
+
+        public WeatherService()
+        {
+            InitializeComponent();
+            timer = new Timer();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            WrittingToFile($"The service was started at {DateTime.Now:dd/MM/yyyy hh:mm:ss}");
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = (1000 * 60/* * 60*/);
+            timer.Enabled = true;
+        }
+
+        protected override void OnStop()
+        {
+            WrittingToFile($"The service was stopped at {DateTime.Now:dd/MM/yyyy hh:mm:ss}");
+        }
+
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WrittingToFile($"The service was executed again at {DateTime.Now:dd/MM/yyyy hh:mm:ss}");
+        }
+
+        private void WrittingToFile(string message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+
+            string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.ToShortDateString().Replace("/", "-") + ".txt";
+
+            if (!File.Exists(filepath))
+            {
+                using (StreamWriter streamWriter = File.CreateText(filepath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+            }
+            else
+            {
+                using (StreamWriter streamWriter = File.AppendText(filepath))
+                {
+                    streamWriter.WriteLine(message);
+                }
+            }
+        }
+
+        public void RunAsConsole(string[] args)
+        {
+            OnStart(args);
+            Console.WriteLine("Service Running as Console Application now.");
+            Console.ReadKey();
+            OnStop();
+        }
+    }
+}
